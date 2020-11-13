3485c2532f462a9e8caaa1ffe1263ade2528f2ea
Artūras Jegorenkovas <jegorenkov@gmail.com>
2020-11-13 16:38:28 +0200

    form styles

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react'
 
 import 'nae-react-core-styles/dist/index.css'
 import {NaeCoreStylesComponent, NaeLayout, NaeNavBar} from "nae-react-core-styles";
+import {Button, Card, Col, Form} from "react-bootstrap";
 
 const App = () => {
   return (
@@ -30,6 +31,86 @@ const App = () => {
         <NaeLayout.NaePageViewLayout leftComponent={"NaePageViewLayout left"}
                                      rightComponent={"NaePageViewLayout right"}/>
         <NaeCoreStylesComponent/>
+
+
+        <hr/>
+
+        <Card>
+          <Card.Body>
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email"/>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password"/>
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Group controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St"/>
+              </Form.Group>
+
+              <Form.Group controlId="formGridAddress2">
+                <Form.Label>Address 2</Form.Label>
+                <Form.Control placeholder="Apartment, studio, or floor"/>
+              </Form.Group>
+
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control/>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control as="select" defaultValue="Choose...">
+                    <option>Choose...</option>
+                    <option>...</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>Zip</Form.Label>
+                  <Form.Control/>
+                </Form.Group>
+              </Form.Row>
+
+
+              <Form.Row>
+                <Form.Group as={Col} sm={3} controlId="formGridCity">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control/>
+                </Form.Group>
+
+                <Form.Group as={Col} sm={3} controlId="formGridState">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control as="select" defaultValue="Choose...">
+                    <option>Choose...</option>
+                    <option>...</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group as={Col} sm={3} controlId="formGridZip">
+                  <Form.Label>Zip</Form.Label>
+                  <Form.Control/>
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Group id="formGridCheckbox">
+                <Form.Check type="checkbox" label="Check me out"/>
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </Fragment>
     </NaeLayout.NaePageWrapper>
   )
